322a3ae30c325fa6e12ee9b4f64ca11afc700ca9
Bret Comnes <bcomnes@gmail.com>
2018-01-02 14:27:42 -0800

    Try putting it in the head

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 var netlifyIdentity = require('./netlify-identity')
 
-console.log(netlifyIdentity)
 
- netlifyIdentity.init({
-   container: "#netlify-modal" // defaults to document.body
- });
+netlifyIdentity.on('init', user => console.log(user))
+netlifyIdentity.on('login', login => console.log(user))
+netlifyIdentity.on('logout', () => console.log("Logged out"))
+netlifyIdentity.on('error', err => console.error("Logged out"))
+netlifyIdentity.on('open', () => console.log("Widget opened"))
+netlifyIdentity.on('close', () => console.log("Widget closed"))
 
- console.log('hi')
 
- // identity.open() // open the modal
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
 
- netlifyIdentity.on('init', user => console.log(user))
- netlifyIdentity.on('login', login => console.log(user))
- netlifyIdentity.on('logout', () => console.log("Logged out"))
- netlifyIdentity.on('error', err => console.error("Logged out"))
- netlifyIdentity.on('open', () => console.log("Widget opened"))
- netlifyIdentity.on('close', () => console.log("Widget closed"))
+  netlifyIdentity.init({
+    container: "#netlify-modal" 
+  });
 
- // Close the modal
- // identity.close()
 
- // Logout the user
- // loginModal.logout();
-
- // Access the underlying gotrue instance.
- // Note that doing things directly through gotrue brings a risk of getting out of
- // sync between your state and the widgets state.
- netlifyIdentity.gotrue
+});
